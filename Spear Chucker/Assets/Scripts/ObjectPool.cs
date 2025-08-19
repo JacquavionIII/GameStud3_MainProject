247886e0690704250fd07599ba5413368739ad54
JacquavionIII <sengoara.lethabo@gmail.com>
2025-08-19 12:30:39 +0200

    setting up player and stuff
    havent added the movement script

---
+++ Spear Chucker/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,35 @@
+
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Pool;
+
+public class ObjectPool : MonoBehaviour
+{
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+//         poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+//         foreach (Pool pool in pools)
+//         {
+//             Queue<GameObject> objectPool = new Queue<GameObject>();
+
+//             for (int i = 0; i < pool size; i++)
+//             {
+//                 GameObject obj = Instantiate(pool prefab);
+//                 obj.SetActive(false);
+//                 objectPool.Enqueue(obj);
+//             }
+
+//             poolDictionary.Add(pool.tag, objectPool);
+//         }
+//     }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
