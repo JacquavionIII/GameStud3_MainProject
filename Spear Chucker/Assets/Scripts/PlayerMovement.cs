a0195e256ad928593ed1fe2fbd13b64954904c0b
JacquavionIII <sengoara.lethabo@gmail.com>
2025-08-19 12:56:53 +0200

    Player Movement Setup
    The player movement is there, just need to add more stuff

---
+++ Spear Chucker/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,144 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerInput))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float speed = 12f;                
+    public float gravity = -9.81f;           
+    public float jumpHeight = 3f; 
+
+    [Header("Ground Check")]
+    public Transform groundCheck;            
+    public float groundDistance = 0.4f;         
+    public LayerMask groundLayer;
+
+    [Header("Camera Settings")]
+    public Transform cameraTransform;
+    public float lookSensitivity = 3f;      // Controling the look sensitivity
+    public float smoothTime = 0.05f;        // How quickly the camera lerps
+    public float minLookY = -60f;           // Clamping the vertical look (up) 
+    public float maxLookY = 60f;            // Clamping the vertical look (down)
+
+    public Rigidbody rb;
+    private Vector2 currentInput;           // Current input from keyboard/gamepad
+    private Vector2 currentLook;            // Current input from mouse/gamepad
+    private Vector2 smoothLook;             // Smoothed look direction
+    private Vector2 lookVelocity;           // Velocity used by SmoothDamp
+    private Vector3 velocity;               // Jump velocity
+    private bool isGrounded;
+    private float camRotationX;             // Vertical camera rotation
+
+    private InputAction moveAction;         // Input action for movement
+    private InputAction lookAction;         // Input action for looking around
+    private InputAction jumpAction;         // Input action for jumping
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        //Get the player's input actions
+        var playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+        lookAction = playerInput.actions["Look"];
+        jumpAction = playerInput.actions["Jump"];
+    }
+
+    void OnEnable()     // Subscribe to input actions when the script is enabled
+    {
+        moveAction.Enable();
+        moveAction.performed += OnMove;
+        moveAction.canceled += OnMove;
+
+        lookAction.Enable();
+        lookAction.performed += OnLook;
+        lookAction.canceled += OnLook;
+
+        jumpAction.Enable();
+        jumpAction.performed += OnJump;
+    }
+
+    void OnDisable()   // Unsubscribe from input actions when the script is disabled
+    {
+        moveAction.performed -= OnMove;
+        moveAction.canceled -= OnMove;
+
+        lookAction.performed -= OnLook;
+        lookAction.canceled -= OnLook;
+
+        jumpAction.performed -= OnJump;
+    }
+
+    // Called whenever Move input changes
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        currentInput = context.ReadValue<Vector2>();
+    }
+
+    // Called whenever Look input changes
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        currentLook = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            // Jump velocity based on physics equation
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    void Update()
+    {
+        // Groundcheck lol
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+        
+        // Reset vertical velocity if grounded and falling
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // small downward force keeps player grounded
+        }
+
+        HandleCameraLook(); //Calling this in Update for smoother camera movement
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();//Rather call this in FixedUpdate for physics-based movement (and Im lowkey experimenting here)
+    }
+
+    void HandleMovement()
+    {
+       // real-time movement cause the orignal one was fucking out and made me tweak a bit....
+        Vector3 move = (transform.right * currentInput.x + transform.forward * currentInput.y).normalized * speed;
+
+        // movement along x with the rb, if this fucks up I'm gonna tweak cause it was working before
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+
+        // Apply gravity & jump (velocity.y is modified in Update or OnJump)
+        velocity.y += gravity * Time.fixedDeltaTime;
+        rb.AddForce(Vector3.up * velocity.y, ForceMode.Acceleration);
+    }
+    
+    void HandleCameraLook()
+    {
+        // Smooth input with Lerp (or SmoothDamp for extra smoothness)
+        smoothLook = Vector2.SmoothDamp(smoothLook, currentLook, ref lookVelocity, smoothTime); //using the ref to keep track of the velocity to make the smoothing work
+
+        // Horizontal rotation (rotate the player body)
+        transform.Rotate(Vector3.up * smoothLook.x * lookSensitivity * Time.deltaTime);
+
+        // Vertical rotation (rotate camera only)
+        camRotationX -= smoothLook.y * lookSensitivity * Time.deltaTime;
+        camRotationX = Mathf.Clamp(camRotationX, minLookY, maxLookY);
+
+        cameraTransform.localRotation = Quaternion.Euler(camRotationX, 0f, 0f);
+    }
+}
